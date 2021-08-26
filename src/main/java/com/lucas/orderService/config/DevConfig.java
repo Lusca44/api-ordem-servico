588598d661221ed8dd7c696924d4c6dfe0b3aab7
Lusca44 <lucasssrodrigues1@gmail.com>
2021-08-25 22:20:47 -0300

    DevConfig and application-dev.properties created - dev profile

--- src/main/java/com/lucas/orderService/config/DevConfig.java
+++ src/main/java/com/lucas/orderService/config/DevConfig.java
@@ -1,6 +1,7 @@
 package com.lucas.orderService.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,16 +9,21 @@ import org.springframework.context.annotation.Profile;
 import com.lucas.orderService.services.DBService;
 
 @Configuration
-@Profile("test")
-public class TestConfig{
-
+@Profile("dev")
+public class DevConfig {
+ 
 	@Autowired
 	private DBService service;
-	
+
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String ddl;
+
 	@Bean
-	public void instaciaDB() {
-		this.service.instanciaDB();
+	public boolean instaciaDB() {
+		if (ddl.equals("create")) {
+			this.service.instanciaDB();
+		}
+		return false;
 	}
-	
 
 }
