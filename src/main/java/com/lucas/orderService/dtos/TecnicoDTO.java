5bf61faf11a47c057425083f37e60e29a65c0fb7
Lusca44 <lucasssrodrigues1@gmail.com>
2021-08-31 22:53:05 -0300

    validation to POST tecnico class (not null)

--- src/main/java/com/lucas/orderService/dtos/TecnicoDTO.java
+++ src/main/java/com/lucas/orderService/dtos/TecnicoDTO.java
@@ -2,6 +2,8 @@ package com.lucas.orderService.dtos;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
 import org.hibernate.validator.constraints.br.CPF;
 
 import com.lucas.orderService.domain.Tecnico;
@@ -10,9 +12,15 @@ public class TecnicoDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	@NotEmpty(message = "O campo NOME é requerido!")
 	private String nome;
+	
 	@CPF
+	@NotEmpty(message = "O campo CPF é requerido!")
 	private String cpf;
+	
+	@NotEmpty(message = "O campo TELEFONE é requerido!")
 	private String telefone;
 	
 	public TecnicoDTO() {
--- src/main/java/com/lucas/orderService/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/lucas/orderService/resources/exceptions/ResourceExceptionHandler.java
@@ -2,6 +2,8 @@ package com.lucas.orderService.resources.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -23,6 +25,17 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> dataIntegritiViolationException(DataIntegritiViolationException obj) {
 		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
 				obj.getMessage());
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> dataIntegritiViolationException(MethodArgumentNotValidException obj) {
+		ValidationError error = new ValidationError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+				"Erro na Validação dos campos!");
+		for(FieldError x : obj.getBindingResult().getFieldErrors()) {
+			error.addError(x.getField(), x.getDefaultMessage());
+		}
 		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
