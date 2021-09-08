27c97bb218b5a798fce986cc7ca3ba05a141e2f4
Lusca44 <lucasssrodrigues1@gmail.com>
2021-09-08 00:02:03 -0300

    api configuration implementation

--- src/main/java/com/lucas/orderService/resources/OrdemServicoResource.java
+++ src/main/java/com/lucas/orderService/resources/OrdemServicoResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.lucas.orderService.dtos.OrdemServicoDTO;
 import com.lucas.orderService.services.OrdemServicoService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/ordemServico")
 public class OrdemServicoResource {
--- src/main/java/com/lucas/orderService/resources/TecnicoResource.java
+++ src/main/java/com/lucas/orderService/resources/TecnicoResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import com.lucas.orderService.domain.Tecnico;
 import com.lucas.orderService.dtos.TecnicoDTO;
 import com.lucas.orderService.services.TecnicoService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/tecnicos")
 public class TecnicoResource {
